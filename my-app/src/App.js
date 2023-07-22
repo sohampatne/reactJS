9537a85eac3b248f0a4de842413f989d9367e266
Soham Vinod Patne <75200736+sohampatne@users.noreply.github.com>
2023-07-22 15:29:58 +0530

    Initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+
+    <div className="App">
+
+      <header className="App-header">
+        <p> <strong> Welcome to my first application in ReactJS.</strong> <br></br>
+          This is an introduction to the basic things you need to know in ReactJS.<br></br><hr></hr>
+          Let's get started.</p>
+      </header>
+
+      <body className='App-body'>
+        <h1>What is ReactJS?</h1>
+        <p>
+          <ul>
+            <li>UI - React is a JavaScript library for building user interfaces.</li>
+            <li>Single-page - React is used to build single-page applications.</li>
+            <li>Reusable components - React allows us to create reusable UI components.</li>
+            <li>Interactive - React makes it painless to create interactive UIs.</li>
+            <li>Dynamic - Like static DHTML, React gets us build dynamic UIs.</li>
+            <li>Flexible - React is a declarative, efficient, and flexible JavaScript library </li>
+          </ul>
+        </p>
+      </body>
+
+    </div>
+  );
+}
+
+export default App;
